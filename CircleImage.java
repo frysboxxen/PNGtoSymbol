71ab115de466356cec247f51a358f2f07606fbd6
flixon <flixon@me.com>
2017-01-10 17:07:51 +0100

    remove java.awt.geom.

--- CircleImage.java
+++ CircleImage.java
@@ -9,7 +9,7 @@ import javax.swing.JTextField;
 import javax.swing.ImageIcon;
 
 import java.awt.*;
-import java.awt.geom.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
